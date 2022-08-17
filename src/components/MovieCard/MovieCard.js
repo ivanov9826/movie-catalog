45c7ec29d7ec25b345a6f7bc44c4b7114790c28d
ivanov9826 <ivanov9826@gmail.com>
2022-08-17 11:31:46 +0300

    Edit css

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -6,9 +6,9 @@ export const MovieCard = ({movie}) => {
     return(
         <div className={styles.cardContent}>
             <img className={styles.img} src={movie.imageUrl} width={'auto'} height={200}/>
-            <h2>{movie.name}</h2>
-            <h6>{movie.author.username}</h6>
-            <Link to='/catalog' >{movie.details}</Link>
+            <h2 className={styles.names}>{movie.name}</h2>
+            <h5 className={styles.names}>{movie.author.username}</h5>
+            <Link to='/catalog' className={styles.detailsButton}>Details</Link>
         </div>
     )
 }
