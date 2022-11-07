92c9d007a5881c8fd510b27ecfce3678dbaf6053
ivanov9826 <ivanov9826@gmail.com>
2022-11-07 21:04:20 +0200

    CSS

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -4,15 +4,9 @@ import styles from "./MovieCard.module.css";
 export const MovieCard = ({ movie }) => {
   return (
     <div className={styles.cardContent}>
-      <img
-        className={styles.img}
-        src={movie.poster}
-        width={"auto"}
-        height={200}
-        alt="Movie Poster"
-      />
-      <h2 className={styles.names}>{movie.title}</h2>
-      <h5 className={styles.names}>{movie.director}</h5>
+      <img className={styles.img} src={movie.poster} alt="Movie Poster" />
+      <h2 className={styles.title}>{movie.title}</h2>
+      <h5 className={styles.director}>{movie.director}</h5>
       <Link to={`/catalog/${movie.id}`} className={styles.detailsButton}>
         Details
       </Link>
