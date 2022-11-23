e98d555786d5e4cd3d3a7cbdd45885aec9ef1260
ivanov9826 <ivanov9826@gmail.com>
2022-11-23 10:23:40 +0200

    Add Link from featured movie to details page

--- src/components/FeaturedMovie/FeaturedMovie.js
+++ src/components/FeaturedMovie/FeaturedMovie.js
@@ -1,20 +1,23 @@
+import { Link } from "react-router-dom";
 import styles from "./FeaturedMovie.module.css";
 
 const FeaturedMovie = ({ movie }) => {
   return (
     <div className={styles.featuredWrapper}>
-      <div className={styles.textWrapper}>
-        <h2>Featured Movie</h2>
-      </div>
-      <div className={styles.backgroundWrapper}>
-        <img src={movie.poster} alt="featured movie poster" />
-      </div>
-      <div>
-        <div className={styles.contentWrapper}>
-          <img src={movie.poster} alt="featured movie poster" />
-          <h3>{movie.title}</h3>
+      <Link to={`/catalog/${movie.id}`}>
+        <div className={styles.textWrapper}>
+          <h2>Featured Movie</h2>
         </div>
-      </div>
+        <div className={styles.backgroundWrapper}>
+          <img src={movie.poster} alt="featured movie poster" />
+        </div>
+        <div>
+          <div className={styles.contentWrapper}>
+            <img src={movie.poster} alt="featured movie poster" />
+            <h3>{movie.title}</h3>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
