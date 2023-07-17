a5d86c20d080c9eb18ee3d72a0a480260bc34527
ivanov9826 <ivanov9826@gmail.com>
2023-07-17 16:14:24 +0300

    Changed movies sorting function

--- src/lib/sortingMovies.js
+++ src/lib/sortingMovies.js
@@ -1,9 +1,9 @@
 const sortingMovies = (movies) => {
-  const sortedMovies = movies.sort((a, b) => {
+  movies.sort((a, b) => {
     return new Date(b.dateAdded) - new Date(a.dateAdded);
   });
 
-  return sortedMovies;
+  return movies;
 };
 
 export default sortingMovies;
